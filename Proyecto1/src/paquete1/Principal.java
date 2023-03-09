4843209a120f3ff9c2e208dea423d8f9d1f1cf02
usuario <usuario@DESKTOP-LJ4PAK7>
2023-03-09 14:26:47 +0100

    Final

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -3,8 +3,9 @@ package paquete1;
 public class Principal {
 	
 	public static void main(String[] args) {
-		System.out.println("Estamos haciendo control de versiones con Git");
-		System.out.println("Estamos haciendo control de versiones con Git otra vez");
+		System.out.println("Estamos muy contentos porque hoy es jueves");
+		System.out.println("Mañana estaremos más contentos porque será viernes");
+		System.out.println("El lunes ya no estaremos tan contentos");
 	}
 
 }
